4077049d0e7042700d325fad6b8e0a1436b26ebf
nina.z@live.se <nina.z@live.se>
2024-11-30 15:25:32 +0100

    skapat funktioner för read och update

--- src/controllers/clothingController.js
+++ src/controllers/clothingController.js
@@ -13,4 +13,34 @@ const createClothing = async (req, res) => {
     }
 }
 
-module.exports = { createClothing }
+const getAllClothes = async (req, res) => {
+    try {
+        const clothes = await Clothing.find()
+        res.status(200).json(clothes)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+const updateClothing = async (req, res)  => {
+    const clothing_id = req.params.clothing_id
+    const { price, size } = req.body 
+    
+    try {
+        const updatedClothing = await Clothing.findByIdAndUpdate(
+            clothing_id,
+            { price, size },
+            { new: true }
+        )
+        if (!updateClothing) {
+            return res.status(404).json({ message: "Clothing not found" })
+        }
+        res.status(200).json(updatedClothing)
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal Server Error")
+    }
+}
+
+module.exports = { createClothing, getAllClothes, updateClothing }
